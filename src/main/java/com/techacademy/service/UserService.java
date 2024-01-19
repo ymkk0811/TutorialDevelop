cd33920776b8524197e6b87c1accbeb4566cfe0b
ymkk0811 <ymkk.hana@docomo.ne.jp>
2024-01-19 17:12:32 +0900

    作り直し

---
+++ src/main/java/com/techacademy/service/UserService.java
@@ -0,0 +1,47 @@
+package com.techacademy.service;
+
+import java.util.List;
+import java.util.Set; // 追加
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.techacademy.entity.User;
+import com.techacademy.repository.UserRepository;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository repository) {
+        this.userRepository = repository;
+    }
+
+    /** 全件を検索して返す */
+    public List<User> getUserList() {
+        // リポジトリのfindAllメソッドを呼び出す
+        return userRepository.findAll();
+    }
+
+    /** Userを1件検索して返す */
+    public User getUser(Integer id) {
+        return userRepository.findById(id).get();
+    }
+
+    /** Userの登録を行なう */
+    @Transactional
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    // ----- 追加:ここから -----
+    /** Userの削除を行なう */
+    @Transactional
+    public void deleteUser(Set<Integer> idck) {
+        for(Integer id : idck) {
+            userRepository.deleteById(id);
+        }
+    }
+    // ----- 追加:ここまで -----
+}
+
